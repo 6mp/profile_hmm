707fff458732d32a1a52b9bbdf136715034f4aa7
matt <30198937+6mp@users.noreply.github.com>
2025-06-25 19:44:59 -0400

    Delete run_student_code.sh

--- run_student_code.sh
+++
@@ -1,8 +0,0 @@
-s=/autograder/source
-for x in $s/Data/PF*hmm; do
-    timeout 120 $s/profile_hmm -m $x -q $s/Data/queries.fas -o $s/Data/`basename $x .hmm`_student_output.txt > $s/Data/`basename $x .hmm`_student_error.log 2>&1
-    if [ $? -eq 124 ]; then
-        echo "Time out after 120 seconds!" >> $s/Data/`basename $x .hmm`_student_error.log
-    fi
-done
-
